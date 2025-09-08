6fd2e7e34a17dec7799f12eba9d216f9ba3dc9ba
AndreaSuDev <andrea.solorzano@yocontigo-it.com>
2025-09-08 17:27:42 -0600

    feat: actions buttons header user

--- app/presentation/usuarios/components/cards/user-header.tsx
+++ app/presentation/usuarios/components/cards/user-header.tsx
@@ -1,5 +1,6 @@
 import type { User } from "@prisma/client";
-import { Mail } from "lucide-react";
+import { Edit, Mail, Trash2 } from "lucide-react";
+import { ActionButton } from "~/shared/components/common/action-button";
 import { Badge } from "~/shared/components/ui/badge";
 import { getUserRoleBadge } from "~/shared/lib/badge-utils";
 import { formatName, getUserInitials } from "~/shared/lib/utils";
@@ -27,7 +28,21 @@ export function UserHeader({ user }: { user: Omit<User, "password"> }) {
         </div>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-3"></div>
+      <div className="flex flex-col sm:flex-row gap-3 sm:ml-auto sm:items-center">
+        <ActionButton
+          icon={<Edit className="w-4 h-4 mr-2" />}
+          text="Editar"
+          variant="bg-primary dark:bg-primary/30 border-secondary dark:border-primary text-white hover:bg-primary/10  hover:text-white dark:hover:bg-primary/10 active:bg-primary/10 dark:active:bg-secondary/10 active:text-black dark:active:text-white active:shadow-inner transition-all duration-200"
+          href={`/usuarios/actualizar/${user.id}`}
+        />
+
+        <ActionButton
+          icon={<Trash2 className="w-4 h-4 mr-2" />}
+          text="Eliminar"
+          onClick={() => {}}
+          variant={`bg-secondary dark:bg-secondary border-secondary dark:border-primary text-white hover:bg-primary/10  hover:text-white dark:hover:bg-primary/10 active:bg-primary/10 dark:active:bg-secondary/10 active:text-black dark:active:text-white active:shadow-inner  flex w-full sm:w-auto items-center justify-center px-4 py-2.5 border rounded-lg transition-all duration-200 `}
+        />
+      </div>
     </div>
   );
 }
