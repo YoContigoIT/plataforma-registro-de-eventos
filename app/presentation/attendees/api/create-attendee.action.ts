4b00ac7052da4d56869db20a653f31b41392fc8c
AndreaSuDev <andrea.solorzano@yocontigo-it.com>
2025-09-10 14:54:57 -0600

    feat: action create attendee

--- app/presentation/attendees/api/create-attendee.action.ts
+++ app/presentation/attendees/api/create-attendee.action.ts
@@ -1,21 +1,95 @@
+import { UserRole } from "@prisma/client";
+import type { CreateRegistrationDto } from "~/domain/dtos/registration.dto";
+import { createUserSchema, type CreateUserDTO } from "~/domain/dtos/user.dto";
+import { generateQRCode, simplifyZodErrors } from "~/shared/lib/utils";
 import type { Route } from "../routes/+types/join";
 
 export const createAttendeeAction = async ({
   request,
+  params,
   context: { repositories },
 }: Route.ActionArgs) => {
   try {
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
 
-    // Aquí puedes agregar validaciones si es necesario
+    const result = createUserSchema.safeParse({
+      email: data.email,
+      name: data.name,
+      phone: data.phone || undefined,
+      role: UserRole.ATTENDEE,
+      password: data.password,
+    });
 
-    // Crear asistente
-    // await repositories.attendeeRepository.create({
-    //   name: data.name as string,
-    //   email: data.email as string,
-    //   eventId: data.eventId as string,
-    // });
+    const ticketsRequested = Number(data.quantity) || 1;
+    if (ticketsRequested <= 0) {
+      return { error: "La cantidad de tickets debe ser mayor a 0." };
+    }
+
+    if (!result.success) {
+      return {
+        error: "Error de validación",
+        errors: simplifyZodErrors<CreateUserDTO>(result.error),
+      };
+    }
+
+    let user = null;
+    // Verificar si el email ya existe
+    user = await repositories.userRepository.findByEmail(result.data.email);
+    if (!user) {
+      user = await repositories.userRepository.create(result.data);
+    }
+    // Obtener evento
+    const eventId = params.eventId;
+    const event = await repositories.eventRepository.findUnique(eventId);
+    if (!event) {
+      return {
+        error: "Evento no encontrado",
+      };
+    }
+
+    const maxTickets = event.maxTickets || 0;
+    const capacity = event.capacity;
+
+    // Contar cuántos tickets ya tiene el usuario en ese event
+    const userTickets =
+      await repositories.registrationRepository.countRegistrations({
+        userId: user.id,
+        eventId: eventId,
+      });
+
+    if (userTickets + ticketsRequested > maxTickets) {
+      return {
+        error: `Solo puedes comprar ${maxTickets} tickets como máximo. 
+                Actualmente tienes ${userTickets}, 
+                intentaste comprar ${ticketsRequested}.`,
+      };
+    }
+    // Tickets totales del evento
+    const totalTickets =
+      await repositories.registrationRepository.countRegistrations({
+        eventId: eventId,
+      });
+
+    // Validación contra capacidad
+    if (totalTickets + ticketsRequested > capacity) {
+      return {
+        error: `El evento alcanzó su capacidad máxima de ${capacity} tickets. 
+                Actualmente hay ${totalTickets} registrados.`,
+      };
+    }
+
+    const registrations: CreateRegistrationDto[] = Array.from({
+      length: ticketsRequested,
+    }).map(() => ({
+      qrCode: generateQRCode(user.id, eventId),
+      userId: user.id,
+      eventId,
+    }));
+
+    await Promise.all(
+      registrations.map((r) => repositories.registrationRepository.create(r))
+    );
 
     return {
       message: "Asistente registrado exitosamente.",
