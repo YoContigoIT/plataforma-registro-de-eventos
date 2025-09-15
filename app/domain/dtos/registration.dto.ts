c8046a382833b996c628a52c8f52e62966ddd9b2
AndreaSuDev <andrea.solorzano@yocontigo-it.com>
2025-09-15 14:24:47 -0600

    fix: changes dto

--- app/domain/dtos/registration.dto.ts
+++ app/domain/dtos/registration.dto.ts
@@ -5,18 +5,24 @@ export const createRegistrationSchema = z.object({
   qrCode: z.string(),
   eventId: z.string(),
   userId: z.string(),
-  status: z.enum(RegistrationStatus).optional().default(RegistrationStatus.PENDING),
+  status: z
+    .enum(RegistrationStatus)
+    .optional()
+    .default(RegistrationStatus.PENDING),
   inviteToken: z.string().optional(),
   invitedAt: z.date().optional(),
   respondedAt: z.date().optional(),
   registeredAt: z.date().optional(),
+  purchasedTickets: z.number().optional(),
 });
 
-export const updateRegistrationSchema = createRegistrationSchema.partial().extend({
-  id: z.uuid({
-    error: "Id inválido",
-  }),
-});
+export const updateRegistrationSchema = createRegistrationSchema
+  .partial()
+  .extend({
+    id: z.uuid({
+      error: "Id inválido",
+    }),
+  });
 
 export type CreateRegistrationDto = z.infer<typeof createRegistrationSchema>;
 export type UpdateRegistrationDto = z.infer<typeof updateRegistrationSchema>;
