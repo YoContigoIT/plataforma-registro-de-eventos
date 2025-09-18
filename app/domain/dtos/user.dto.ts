8085a498b96521df1664f4a5a63fab0454f97b02
AndreaSuDev <andrea.solorzano@yocontigo-it.com>
2025-09-17 19:00:31 -0600

    changes: user dto

--- app/domain/dtos/user.dto.ts
+++ app/domain/dtos/user.dto.ts
@@ -2,10 +2,12 @@ import { UserRole } from "@prisma/client";
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-  email: z.email({
-    error: "Correo electrónico inválido",
-    pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-  }).trim(),
+  email: z
+    .email({
+      error: "Correo electrónico inválido",
+      pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    })
+    .trim(),
   name: z.string().trim(),
   company: z.string().trim().nullable().optional(),
   title: z.string().trim().nullable().optional(),
@@ -14,8 +16,11 @@ export const createUserSchema = z.object({
       error: "El número de teléfono es requerido",
     })
     .trim()
-    .regex(/^\d+$/, { message: "El número de teléfono solo debe contener dígitos" })
-    .optional(),
+    .transform((val) => (val === "" ? undefined : val)) // 🔑
+    .optional()
+    .refine((val) => !val || /^\d+$/.test(val), {
+      message: "El número de teléfono solo debe contener dígitos",
+    }),
   password: z.string().trim().optional(),
   role: z.enum(UserRole).optional(),
 });
@@ -23,14 +28,28 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema.partial();
 
 export const changePasswordSchema = z.object({
-  currentPassword: z.string({
-    error: "La contraseña actual es requerida.",
-  }).trim(),
-  newPassword: z.string({
-    error: "La nueva contraseña es requerida.",
-  }).trim(),
+  currentPassword: z
+    .string({
+      error: "La contraseña actual es requerida.",
+    })
+    .trim(),
+  newPassword: z
+    .string({
+      error: "La nueva contraseña es requerida.",
+    })
+    .trim(),
+});
+
+export const createGuestSchema = createUserSchema.extend({
+  eventId: z.string({
+    error: "El ID del evento es requerido",
+  }),
+  quantity: z
+    .string({ error: "La cantidad de invitados es requerida" })
+    .min(1, "La cantidad de invitados debe ser al menos 1"),
 });
 
 export type ChangePasswordDTO = z.infer<typeof changePasswordSchema>;
 export type CreateUserDTO = z.infer<typeof createUserSchema>;
 export type UpdateUserDTO = z.infer<typeof updateUserSchema>;
+export type CreateGuestDTO = z.infer<typeof createGuestSchema>;
