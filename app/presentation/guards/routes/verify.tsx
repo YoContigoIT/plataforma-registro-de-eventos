2099ed9df130b9d7bf1f4da638bea92e6e13ad68
AndreaSuDev <andrea.solorzano@yocontigo-it.com>
2025-11-10 20:53:01 -0600

    fix(guard): Expandir la vista del guardia extendiendo lo blanco verificar registro

--- app/presentation/guards/routes/verify.tsx
+++ app/presentation/guards/routes/verify.tsx
@@ -95,7 +95,7 @@ export default function Verify() {
 
   return (
     <div className="bg-background flex min-h-svh bg-gradient-to-t from-primary/20 to-secondary/20 dark:from-primary/30 dark:to-secondary/30">
-      <div className="w-full  flex flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full  flex flex-col items-center justify-center md:p-10">
         <div className="w-full max-w-5xl">
           <VerifyRegistration
             invite={data?.invite}
